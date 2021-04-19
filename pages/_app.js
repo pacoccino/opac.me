d7e62a1c23d226971a3944c9f884f370717e96e4
Pacien Boisson <pakokrew@gmail.com>
2021-04-19 14:03:14 +0400

    fix fa

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/global.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config as faConfig } from '@fortawesome/fontawesome-svg-core'
+faConfig.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
